2b1b30c8110feaf216bb764142729bffbf446685
AlulCode45 <alulgans70@gmail.com>
2024-12-10 22:18:27 +0700

    add: fetch data teacher and students on Member.jsx

--- src/components/room/ListMember.jsx
+++ src/components/room/ListMember.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const ListMember = (props) => {
-  const {member} = props
+  const {member,key} = props
   const image = 'https://avatarfiles.alphacoders.com/374/thumb-1920-374880.png'
   
   return (
     <>
-        <div className='flex items-center gap-x-2 py-2 px-4'>
+        <div className='flex items-center gap-x-2 py-2 px-4' key={key}>
             <img className='w-[50px] h-[50px] rounded-full' src={image} alt='profile'/>
-            <p className='text-sm font-normal'>{member}</p>
+            <p className='text-sm font-normal'>{member.name}</p>
         </div>
     </>
   )
--- src/pages/room/Member.jsx
+++ src/pages/room/Member.jsx
@@ -1,22 +1,44 @@
 import ListMember from '@/components/room/ListMember'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useParams} from "react-router-dom";
+import {showClassroom} from "@/services/ClassroomService.jsx";
 
 const Member = () => {
-    const members = ['Alexia Candra', 'Albert Einsten', 'Nicola Tesla', 'Isaac Newton']
+    const {id} = useParams()
+    const [members, setMembers] = useState([])
+    const [teacher,setTeacher] = useState([])
+
+    useEffect(() => {
+        const detailClass = async () => {
+            return await showClassroom(id)
+                .then(response => {
+                    setMembers(response.data?.students)
+                    setTeacher(response.data?.teacher)
+                })
+        }
+
+        detailClass().catch(error => {
+            console.log(error)
+        })
+    }, [id]);
+
     return (
         <>
             <div>
                 <h1 className='text-xl font-semibold'>Pengajar</h1>
                 <div className='grid grid-cols-2 gap-4 py-6'>
-                    <ListMember member={'Alexander'} />
+                    <ListMember member={teacher}/>
                 </div>
-            </div>  
-            <div>
+                <div>
+                </div>
                 <h1 className='text-xl font-semibold'>Anggota Kelas</h1>
                 <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 py-6'>
-                    {members.map((member, index) => (
-                        <ListMember member={member} />
-                    ))}
+                    {
+                        members ?
+                            members?.map((member, index) => (
+                                <ListMember member={member} key={index}/>
+                            )) : 'Loading...'
+                    }
                 </div>
             </div>
         </>
