a2c16760bd9a058c7de89025236f05ea1552f7fc
AlulCode45 <alulgans70@gmail.com>
2024-12-10 17:10:21 +0700

    update: get data banner from props data

--- src/components/ui/Banner.jsx
+++ src/components/ui/Banner.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
-const Banner = () => {
-  return (
-    <>
-        <div className='bg-green-600 p-6 rounded-lg'>
-            <div className="flex flex-col justify-between min-h-[200px]">
-                <h1 className='text-3xl text-white font-semibold'>Biologi</h1>
-                <p className='text-lg text-white font-normal'>Alicia Friscilla</p>
+const Banner = (props) => {
+    const {data} = props;
+    return (
+        <>
+            <div className={`${data?.bg_tw_class} p-6 rounded-lg`}>
+                <div className="flex flex-col justify-between min-h-[200px]">
+                    <h1 className='text-3xl text-white font-semibold'>{data?.class_name}</h1>
+                    <p className={'text-white'}>{data?.description}</p>
+                    <p className='text-lg text-white font-normal'>{data?.teacher?.name}</p>
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Banner
