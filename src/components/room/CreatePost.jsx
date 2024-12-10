7b302f7bc0521a2a3e0c6d26610db14bf3bc14c6
frmnxhken <frmnxhken@gmail.com>
2024-12-10 19:08:35 +0700

    Making disable enable button

--- src/components/room/CreatePost.jsx
+++ src/components/room/CreatePost.jsx
@@ -3,6 +3,7 @@ import Button from '../ui/Button'
 
 const CreatePost = () => {
   const [active, setActive] = useState(false)
+  const [content, setContent] = useState('')
   
   return (
     <>
@@ -12,12 +13,21 @@ const CreatePost = () => {
 
         {active ? 
         <div className='py-6 px-4 border border-gray-300 rounded-lg shadow-sm'>
-            <textarea className='resize-none w-full bg-slate-100 outline-none p-4 text-sm h-[150px] rounded-md'></textarea>
+            <textarea 
+                    onChange={e => setContent(e.target.value)} 
+                    className='resize-none w-full bg-slate-100 outline-none p-4 text-sm h-[150px] rounded-md'>
+            </textarea>
             <div className='flex justify-between items-center'>
                 <div className='bg-slate-200 w-[200px] h-[20px]'></div>
                 <div className='flex items-center gap-x-2'>
-                    <Button onClick={() => setActive(!active)} type='secondary' text='Batal'/>
-                    <Button type='disable' text='Posting'/>
+                    <Button 
+                        onClick={() => setActive(!active)} 
+                        type='secondary' 
+                        text='Batal'/>
+                    <Button 
+                        disabled={content.length < 1 ? 'disabled' : ''}
+                        type={content.length < 1 ? 'disable' : 'primary'} 
+                        text='Posting'/>
                 </div>
             </div>
         </div> : <></>}
