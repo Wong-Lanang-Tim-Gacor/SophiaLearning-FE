45ada54bcb5fa2c37696c8eaba219b1c2d45646a
AlulCode45 <alulgans70@gmail.com>
2024-12-11 21:33:55 +0700

    update: updating position content on banner

--- src/components/ui/Banner.jsx
+++ src/components/ui/Banner.jsx
@@ -5,10 +5,13 @@ const Banner = (props) => {
     return (
         <>
             <div className={`${data?.bg_tw_class} p-6 rounded-lg`}>
-                <div className="flex flex-col justify-between min-h-[200px]">
-                    <h1 className='text-3xl text-white font-semibold'>{data?.class_name}</h1>
-                    <p className={'text-white'}>{data?.description}</p>
-                    <p className='text-lg text-white font-normal'>{data?.teacher?.name}</p>
+                <div className="flex flex-col justify-end min-h-[200px]">
+                    <div className="flex justify-between">
+                        <div className="class-info">
+                            <h1 className='text-3xl text-white font-semibold'>{data?.class_name}</h1>
+                            <p className={'text-white'}>{data?.description}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
