502b9889854616472dee2dc83e51ea39d6952149
AlulCode45 <alulgans70@gmail.com>
2024-12-13 22:48:17 +0700

    update:change api url

--- src/utils/Constant.jsx
+++ src/utils/Constant.jsx
@@ -1,2 +1,2 @@
-// export const BASE_API = 'https://b771-114-79-22-70.ngrok-free.app/api'
-export const BASE_API = 'http://localhost:8000/api'
+export const BASE_API = 'https://b771-114-79-22-70.ngrok-free.app/api'
+// export const BASE_API = 'http://localhost:8000/api'
