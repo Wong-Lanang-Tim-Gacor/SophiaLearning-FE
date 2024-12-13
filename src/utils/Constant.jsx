054fcdbb85556641180df04c161eb870bf53c946
frmnxhken <frmnxhken@gmail.com>
2024-12-14 00:58:29 +0700

    Change skleton card

--- src/utils/Constant.jsx
+++ src/utils/Constant.jsx
@@ -1,2 +1,2 @@
-export const BASE_API = 'https://726c-139-228-41-125.ngrok-free.app/api'
+export const BASE_API = 'https://1807-139-228-41-125.ngrok-free.app/api'
 // export const BASE_API = 'http://localhost:8000/api'
