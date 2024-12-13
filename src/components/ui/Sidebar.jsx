50340b89e28dc0d1722c8dbab342ddc34c3e93f7
AlulCode45 <alulgans70@gmail.com>
2024-12-13 21:58:20 +0700

    update: adding skeleton on sidebar

--- src/components/ui/Sidebar.jsx
+++ src/components/ui/Sidebar.jsx
@@ -2,7 +2,7 @@ import React, {useContext, useEffect, useState} from 'react'
 import MenuContext from '@/contexts/MenuContext'
 import {Link, useNavigate} from 'react-router-dom'
 import {getClassroom} from "@/services/ClassroomService.jsx";
-import { TextSlice } from '@/utils/FormattingString';
+import {TextSlice} from '@/utils/FormattingString';
 
 const Sidebar = () => {
     const {active} = useContext(MenuContext)
@@ -31,7 +31,7 @@ const Sidebar = () => {
         const getDataClassroom = async () => {
             return await getClassroom()
                 .then(response => {
-                    setClassrooms(response.slice(0,5))
+                    setClassrooms(response.slice(0, 5))
                 })
         }
         getDataClassroom().catch(error => console.log(error))
@@ -44,9 +44,10 @@ const Sidebar = () => {
                     <div className='border-b border-gray-300 pb-3'>
                         {mainLinks.map((link, index) => (
                             <div key={index}
-                                 className='flex items-center gap-x-4 pl-6 py-3 hover:bg-gray-100 rounded-r-2xl w-[95%] cursor-pointer' onClick={() => {
+                                 className='flex items-center gap-x-4 pl-6 py-3 hover:bg-gray-100 rounded-r-2xl w-[95%] cursor-pointer'
+                                 onClick={() => {
                                      navigate(link.path)
-                            }}>
+                                 }}>
                                 <div className='w-[30px] text-center'>
                                     <i className={`${link.icon} text-lg`}></i>
                                 </div>
@@ -69,12 +70,18 @@ const Sidebar = () => {
                                         </div>
                                         <p className='text-sm font-medium'>{room.class_name}</p>
                                     </div>
-                                )) : <div className='flex items-center gap-x-4 pl-6 py-3 bg-slate-200 rounded-r-2xl w-[95%] cursor-pointer'>
-                                    <div className={`w-[30px] h-[30px] rounded-full bg-slate-300 flex items-center justify-center`}>
-                                        <p className='text-white text-xs font-bolder'></p>
-                                    </div>
-                                    <p className='text-sm font-medium bg-slate-300 w-22 h-7'></p>
-                                </div>
+                                )) : (
+                                    [1, 2, 3, 4].map(() => (
+                                        <div
+                                            className='flex items-center gap-x-4 pl-6 py-3 bg-slate-200 rounded-r-2xl w-[95%] cursor-pointer my-2'>
+                                            <div
+                                                className={`w-[30px] h-[30px] rounded-full bg-slate-300 flex items-center justify-center`}>
+                                                <p className='text-white text-xs font-bolder'></p>
+                                            </div>
+                                            <p className='text-sm font-medium bg-slate-300 w-22 h-7'></p>
+                                        </div>
+                                    ))
+                                )
                         }
                     </div>
                 </div>
@@ -89,7 +96,7 @@ const Sidebar = () => {
                             <div key={index}
                                  className='flex items-center gap-x-4 pl-6 py-3 hover:bg-gray-100 rounded-r-2xl w-[95%]'>
                                 <div className='w-[30px] text-center'>
-                                <i className={`${link.icon} text-lg`}></i>
+                                    <i className={`${link.icon} text-lg`}></i>
                                 </div>
                                 <Link className='text-sm' to={link.path}>{link.name}</Link>
                             </div>
@@ -100,16 +107,28 @@ const Sidebar = () => {
                             classrooms ?
                                 classrooms?.map((room, index) => (
                                     <div key={index}
-                                         className='flex items-center gap-x-4 pl-6 py-3 hover:bg-gray-100 rounded-r-2xl w-[95%]' onClick={() => {
+                                         className='flex items-center gap-x-4 pl-6 py-3 hover:bg-gray-100 rounded-r-2xl w-[95%]'
+                                         onClick={() => {
                                              navigate(`/room/${room.id}`)
-                                    }}>
+                                         }}>
                                         <div key={index}
                                              className={`w-[30px] h-[30px] rounded-full ${room.bg_tw_class} flex items-center justify-center`}>
                                             <p className='text-white text-xs font-bolder'>{room.class_name.split(' ').map(word => word[0]).join('')}</p>
                                         </div>
                                         <p className='text-sm font-medium'>{room.class_name}</p>
                                     </div>
-                                )) : 'Loading ...'
+                                )) : (
+                                    [1, 2, 3, 4].map(() => (
+                                        <div
+                                            className='flex items-center gap-x-4 pl-6 py-3 bg-slate-200 rounded-r-2xl w-[95%] cursor-pointer  my-2'>
+                                            <div
+                                                className={`w-[30px] h-[30px] rounded-full bg-slate-300 flex items-center justify-center`}>
+                                                <p className='text-white text-xs font-bolder'></p>
+                                            </div>
+                                            <p className='text-sm font-medium bg-slate-300 w-22 h-7'></p>
+                                        </div>
+                                    ))
+                                )
                         }
                     </div>
                 </div>
