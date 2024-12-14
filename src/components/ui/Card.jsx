ba8e49b2f7cecdf73d9e9a8ab28088f529f25100
AlulCode45 <alulgans70@gmail.com>
2024-12-14 10:46:27 +0700

    add: adding notif success copy link

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,21 +1,40 @@
-import React, {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {toast} from "react-hot-toast";
 
 const Card = (props) => {
-    const {data} = props;
+    const { data } = props;
     const navigate = useNavigate();
-    const [showMenu,setShowMenu] = useState(false)
+    const [showMenu, setShowMenu] = useState(false);
+    const [linkCopied, setLinkCopied] = useState(false);
+
+    const handleCopyLink = () => {
+        const link = `${window.location.origin}/room/${data.id}`; // Mengambil link halaman detail berdasarkan ID
+        navigator.clipboard.writeText(link)  // Menyalin link ke clipboard
+            .then(() => {
+                toast.success('Sukses menyalin link!')
+                setLinkCopied(true);  // Menandakan bahwa link berhasil disalin
+                setTimeout(() => setLinkCopied(false), 2000);  // Reset status setelah 2 detik
+            })
+            .catch(err => {
+                toast.error('Gagal menyalin link!');
+            });
+    };
+
     return (
         <>
             <div {...props} className='border border-gray-300 rounded-lg hover:shadow-md cursor-pointer'>
                 <div className="relative">
-                    <button onClick={() => setShowMenu(!showMenu)}
-                            className='p-2 cursor-pointer grid place-content-center absolute right-3 top-2 text-white z-10'>
+                    <button
+                        onClick={() => setShowMenu(!showMenu)}
+                        className='p-2 cursor-pointer grid place-content-center absolute right-3 top-2 text-white z-10'>
                         <i className='fas fa-ellipsis-v text-lg'></i>
                     </button>
                     <div
                         className={`${showMenu ? 'scale-1' : 'scale-0'} rounded-md transition-all border border-gray-300 shadow-md absolute p-4 w-[150px] right-7 top-3 space-y-4 bg-white text-sm`}>
-                        <button className={'block'}>Salin Link</button>
+                        <button className={'block'} onClick={handleCopyLink}>
+                            {linkCopied ? 'Link Disalin!' : 'Salin Link'}
+                        </button>
                         <button className={'block'}>Edit</button>
                         <button className={'block'}>Arsip</button>
                     </div>
@@ -29,7 +48,7 @@ const Card = (props) => {
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default Card
+export default Card;
