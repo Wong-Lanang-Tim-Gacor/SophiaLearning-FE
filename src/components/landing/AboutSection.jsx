1121479ec41762429456490a571aaa8e9b2442b9
frmnxhken <frmnxhken@gmail.com>
2024-12-14 13:44:38 +0700

    Add animation

--- src/components/landing/AboutSection.jsx
+++ src/components/landing/AboutSection.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import ExampleImage from '@/assets/images/homepage.png'
+import React, {useRef} from 'react'
+import { useScroll, motion, useTransform } from 'framer-motion';
+import ExampleImage from '../../assets/images/homepage.png'
 
-const AboutSection = () => {
+const AboutLanding = () => {
+    const ref = useRef(null)
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ['start end', 'end start'],
+    })
+
+    const widthScreen = window.innerWidth
+    const scale = useTransform(scrollYProgress, [0.3, 1], [1, widthScreen > 680 ? 0.7 : 0.9])
+
     return (
         <>
-            <div className='h-[200vh]'>
+            <div ref={ref} className='h-[250vh] relative'>
                 <div className='flex flex-col h-full items-center justify-center'>
-                    <h1 className='text-6xl font-bold text-center'>Apa Itu Sophia?</h1>
-                    <p className='text-lg text-gray-500 w-3/5 text-center mt-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio neque accusantium modi odit voluptatem animi qui fugit nihil maxime minima numquam deserunt ipsa sit dicta omnis, fuga possimus cumque harum.</p>
-                    <img className='w-[80%] mx-auto rounded-lg border border-gray-300 shadow-md mt-24' src={ExampleImage} alt="ss" />
+                    <h1 className='text-4xl sm:text-6xl font-bold text-center'>Apa Itu Sophia?</h1>
+                    <p className='text-sm sm:text-lg text-gray-500 w-3/4 sm:w-3/6 text-center mt-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio neque accusantium modi odit voluptatem animi qui fugit nihil maxime minima numquam deserunt ipsa sit dicta omnis, fuga possimus cumque harum.</p>
+                    <motion.img style={{scale}} className='sticky top-[10%] origin-center w-full mx-auto rounded-lg border border-gray-300 shadow-md mt-24' src={ExampleImage} alt="ss" />
                 </div>
             </div>
         </>
     )
 }
 
-export default AboutSection
+export default AboutLanding
